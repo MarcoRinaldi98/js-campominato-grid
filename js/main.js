9f9c7cc6b0e1f95d23d9630d907bdbc6fa7cf650
Marco Rinaldi <marcorinaldi98@libero.it>
2023-03-03 15:44:53 +0100

    Esercizio quasi terminato con anche i bonus

--- js/main.js
+++ js/main.js
@@ -6,37 +6,77 @@
 const gridDom = document.getElementById('grid');
 //identifico il pulsante per generare la griglia
 const play = document.getElementById('generator');
+//identifico la critta di info per l'utente
+const info = document.getElementById('info');
+//identifico la scelta dell'utente
+const scelta = document.getElementById('difficulty');
+
+let numeriProgressivi = [];
 
 //evento per generare la griglia al click del pulsante play
 play.addEventListener('click',
     function () {
-        gridDom.classList.remove('d-none');
+        info.classList.add('d-none');
+        console.log(scelta.value);
+        if (scelta.value == 1) {
+            for (let i = 1; i <= 100; i++) {
+                //creo i quadrati da inserire nella griglia
+                const currentSquare = createNewSquare(i);
+                currentSquare.classList.add('ms_square_1');
+            
+                //evento per il click del quadratino
+                currentSquare.addEventListener('click',
+                    function() {
+                        this.classList.toggle('clicked');
+                        console.log('hai cliccato sul numero', i);
+                    }
+                );
+                gridDom.append(currentSquare);
+            }
+        } else if (scelta.value == 2) {
+            for (let i = 1; i <= 81; i++) {
+                //creo i quadrati da inserire nella griglia
+                const currentSquare = createNewSquare(i);
+                currentSquare.classList.add('ms_square_2');
+            
+                //evento per il click del quadratino
+                currentSquare.addEventListener('click',
+                    function() {
+                        this.classList.toggle('clicked');
+                        console.log('hai cliccato sul numero', i);
+                    }
+                );
+                gridDom.append(currentSquare);
+            }
+        } else {
+            for (let i = 1; i <= 49; i++) {
+                //creo i quadrati da inserire nella griglia
+                const currentSquare = createNewSquare(i);
+                currentSquare.classList.add('ms_square_3');
+            
+                //evento per il click del quadratino
+                currentSquare.addEventListener('click',
+                    function() {
+                        this.classList.toggle('clicked');
+                        console.log('hai cliccato sul numero', i);
+                    }
+                );
+                gridDom.append(currentSquare);
+            }
+        }
     }
 );
 
-//ciclo per la creazione di 100 quadrati
-for (let i = 0; i < 100; i++) {
-
-    //creo i quadrati da inserire nella griglia
-    const currentSquare = createNewSquare();
-
-    //evento per il click del quadratino
-    currentSquare.addEventListener('click',
-        function() {
-            this.classList.toggle('clicked');
-            console.log('hai cliccato sul numero', newValidRandomNumber);
-        }
-    );
-    gridDom.append(currentSquare);
-}
 
 
 
 //funzione per creare un quadratino
-function createNewSquare() {
+function createNewSquare(number) {
     const currentElement = document.createElement('div');
     currentElement.classList.add('ms_square');
-
+    currentElement.innerHTML = number;
+    
     return currentElement;
 }
+
 
